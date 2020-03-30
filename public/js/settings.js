f6c2b25b905e04f6dffd92a749fb17e22684aa87
quantumVector <payned@mail.ru>
2020-03-30 19:04:38 +0300

    Рефакторинг когда в settings.js

--- public/js/settings.js
+++ public/js/settings.js
@@ -2,65 +2,70 @@
 
 'use strict';
 
-const everyday = document.getElementById('everyday');
-const certainDays = document.getElementsByName('period');
+class Settings {
+  static togglePeriod() {
+    const everyday = document.getElementById('everyday');
+    const certainDays = document.getElementsByName('period');
 
-everyday.addEventListener('click', () => {
-  if (everyday.checked) {
-    for (let i = 1; i <= 7; i++) {
-      certainDays[i].disabled = 1;
-      certainDays[i].checked = 0;
-    }
-  } else {
-    for (let i = 1; i <= 7; i++) {
-      certainDays[i].disabled = 0;
+    if (everyday.checked) {
+      for (let i = 1; i <= 7; i++) {
+        certainDays[i].disabled = 1;
+        certainDays[i].checked = 0;
+      }
+    } else {
+      for (let i = 1; i <= 7; i++) {
+        certainDays[i].disabled = 0;
+      }
     }
   }
-});
 
-async function renderUserActions() {
-  const response = await fetch('/getdata');
-  let data;
+  async getData() {
+    const response = await fetch('/getdata');
 
-  if (response.ok) {
-    data = await response.json();
-  } else {
-    throw new Error(`Возникла проблема с fetch запросом. ${response.status}`);
+    if (response.ok) {
+      this.data = await response.json();
+    } else {
+      throw new Error(`Возникла проблема с fetch запросом. ${response.status}`);
+    }
   }
 
-  const container = document.getElementsByClassName('actions-box')[0];
+  async renderActions() {
+    const container = document.getElementsByClassName('actions-box')[0];
 
-  for (const action of data) {
-    const div = document.createElement('div');
-    const name = document.createElement('h2');
-    const box = document.createElement('div');
-    const boxTitle = document.createElement('h3');
-    const debt = document.createElement('p');
+    await this.getData();
 
-    container.appendChild(div);
-    div.classList.add('action-item');
-    div.appendChild(name);
-    name.classList.add('action-name');
-    div.appendChild(boxTitle);
-    boxTitle.innerText = 'Периодичность:';
-    div.appendChild(box);
-    box.classList.add('action-days');
-    div.appendChild(debt);
+    for (const action of this.data) {
+      const div = document.createElement('div');
+      const name = document.createElement('h2');
+      const box = document.createElement('div');
+      const boxTitle = document.createElement('h3');
+      const debt = document.createElement('p');
 
+      container.appendChild(div);
+      div.classList.add('action-item');
+      div.appendChild(name);
+      name.classList.add('action-name');
+      div.appendChild(boxTitle);
+      boxTitle.innerText = 'Периодичность:';
+      div.appendChild(box);
+      box.classList.add('action-days');
+      div.appendChild(debt);
+
+      this.constructor.embedData(action, name, box, debt);
+    }
+  }
+
+  static embedData(action, name, box, debt) {
     for (const key in action) {
       if ({}.hasOwnProperty.call(action, key)) {
+        /* eslint-disable no-param-reassign */
+        // eslint-disable-next-line prefer-destructuring
+        const days = action[key].params.days;
+
         name.innerText = key;
 
-        if (action[key].params.days === 'everyday') {
-          box.innerText = 'Каждый день';
-        }
-
-        if (Array.isArray(action[key].params.days)) {
-          console.log(action[key].params.days);
-
-          box.innerText = action[key].params.days.join(', ');
-        }
-
+        if (action[key].params.days === 'everyday') box.innerText = 'Каждый день';
+        if (Array.isArray(action[key].params.days)) box.innerText = days.join(', ');
         if (action[key].params.debt) {
           debt.innerText = 'Долги учитываются';
         } else {
@@ -71,4 +76,10 @@ async function renderUserActions() {
   }
 }
 
-renderUserActions();
+const settings = new Settings();
+
+settings.renderActions();
+
+everyday.addEventListener('click', () => {
+  settings.togglePeriod();
+});
