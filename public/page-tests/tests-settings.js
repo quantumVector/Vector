343ed7fc9dbe90d3aa4361617b6bbd75baa7c04c
quantumVector <payned@mail.ru>
2020-03-31 02:03:47 +0300

    Добавлено три новых теста settings

--- public/page-tests/tests-settings.js
+++ public/page-tests/tests-settings.js
@@ -31,25 +31,79 @@ describe('Тестирование страницы "Настройки"', () =>
   });
 
   describe('Тестирование блока с действиями', () => {
-    it('Блок должен содержать все действия, которые были созданы пользователем', () => {
+    const everyday = document.getElementById('everyday');
+    const certainDays = document.getElementsByName('period');
+    let div;
+    let dataTester;
+
+    before(async () => {
+      const settings = new Settings();
+
+      div = document.createElement('div');
+      emptyDiv = document.createElement('div');
+      settings.getData = async function getData() {
+        const response = await fetch('/getdata-test');
+
+        if (response.ok) {
+          this.data = await response.json();
+        } else {
+          throw new Error(`Возникла проблема с fetch запросом. ${response.status}`);
+        }
+      };
+
+      settings.renderActions(div);
+
+      const response = await fetch('/getdata-test');
+
+      if (response.ok) {
+        dataTester = await response.json();
+      } else {
+        throw new Error(`Возникла проблема с fetch запросом. ${response.status}`);
+      }
     });
 
-    it('В блоке должна отображаться надпись об отсутсвии действий, если пользователь их не создавал', () => {
+    it('Блок должен содержать все действия, которые были созданы пользователем', () => {
+      assert.equal(dataTester.length, div.children.length);
+    });
+
+    it('В блоке должна отображаться надпись об отсутсвии действий, если пользователь их не создавал', async () => {
+      const emptyDiv = document.createElement('div');
+      const settings2 = new Settings();
+
+      settings2.getData = async function getData() {
+        this.data = [];
+      };
+
+      await settings2.renderActions(emptyDiv);
+
+      const emptyMsg = emptyDiv.getElementsByClassName('empty-msg')[0];
+
+      assert.equal(emptyMsg.innerText, 'Вы ещё не создали ни одного действия');
     });
 
     it('Название действия должно отображаться в соответствующем ему блоке', () => {
+      for (let i = 1; i < settings.data.length; i++) {
+        const nameAction = div.getElementsByClassName('action-name')[i];
+        const key = Object.keys(dataTester[i])[0];
+
+        assert.equal(nameAction.innerText, key);
+      }
     });
 
     it('Периодичность должна отображаться в соответствующем ему блоке', () => {
+      assert();
     });
 
     it('Должна отображаться надпись "Каждый день", если в действии был указан соответсвущий пункт', () => {
+      assert();
     });
 
     it('Должныо ображаться дни, которые были выбраны для действия', () => {
+      assert();
     });
 
     it('Информация об учёте долгов должна отображаться в соответствующем ему блоке', () => {
+      assert();
     });
   });
 });
