ead949449c71053ef54ac8bdb4ce1145f2bd7de4
quantumVector <payned@mail.ru>
2020-05-23 02:19:43 +0300

    Добавлен текст и стили в мануал

--- routes/get-manual.js
+++ routes/get-manual.js
@@ -6,6 +6,10 @@ router.get('/manual', (req, res) => {
   res.render('manual', {
     title: 'Руководство',
     isManual: true,
+    style: {
+      desk: 'css/manual.css',
+      mob: 'css/manual-mob.css',
+    },
   });
 });
 
